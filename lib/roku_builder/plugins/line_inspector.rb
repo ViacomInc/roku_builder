d9cc2d5b39e83de2542ff1653cc4280191b2f488
Charles Greene <charles.greene@redspace.com>
2021-01-22 11:03:34 -0400

    Update to allow for multiline linter checks

--- lib/roku_builder/plugins/line_inspector.rb
+++ lib/roku_builder/plugins/line_inspector.rb
@@ -13,38 +13,49 @@ module RokuBuilder
       File.open(file_path) do |file|
         in_xml_comment = false
         indent_inspector = IndentationInspector.new(rules: @indent_config, path: file_path) if @indent_config
+        full_file = []
+        file_no_comments = []
+        lines_to_ignore = []
         file.readlines.each_with_index do |line, line_number|
           full_line = line.dup
-          line = line.partition("'").first if file_path.end_with?(".brs")
+          line.gsub!(/'.*/, "") if file_path.end_with?(".brs")
           if file_path.end_with?(".xml")
             if in_xml_comment
               if line.gsub!(/.*-->/, "")
                 in_xml_comment = false
               else
-                line = ""
+                line = "\n"
               end
             end
             line.gsub!(/<!--.*-->/, "")
             in_xml_comment = true if line.gsub!(/<!--.*/, "")
           end
           indent_inspector.check_line(line: full_line, number: line_number, comment: in_xml_comment) if indent_inspector
-          @inspector_config.each do |line_inspector|
-            line_to_check = line
-            line_to_check = full_line if line_inspector[:include_comments]
-            match  = nil
-            if line_inspector[:case_sensitive]
-              match = /#{line_inspector[:regex]}/.match(line_to_check)
-            else
-              match = /#{line_inspector[:regex]}/i.match(line_to_check)
-            end
-            if match
-              unless /'.*ignore-warning/i.match(full_line)
-                add_warning(inspector: line_inspector, file: file_path, line: line_number, match: match)
-              end
+          if  /'.*ignore-warning/i.match(full_line)
+            lines_to_ignore.push line_number
+          end
+          full_file.push(full_line)
+          file_no_comments.push(line)
+        end
+        @warnings += indent_inspector.warnings if indent_inspector
+        no_comments = file_no_comments.join("")
+        file = full_file.join("")
+        @inspector_config.each do |line_inspector|
+          to_check = no_comments
+          to_check = file if line_inspector[:include_comments]
+          match  = nil
+          if line_inspector[:case_sensitive]
+            match = /#{line_inspector[:regex]}/.match(to_check)
+          else
+            match = /#{line_inspector[:regex]}/i.match(to_check)
+          end
+          if match
+            line_number = to_check[0..match.begin(0)].split("\n", -1).count - 1
+            unless lines_to_ignore.include?(line_number)
+              add_warning(inspector: line_inspector, file: file_path, line: line_number, match: match)
             end
           end
         end
-        @warnings += indent_inspector.warnings if indent_inspector
       end
       @warnings
     end
